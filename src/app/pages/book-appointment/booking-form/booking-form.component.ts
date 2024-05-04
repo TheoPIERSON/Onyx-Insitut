6f396d5a96d0b8148163a70b76c4294d8fb0ba96
Theo Pierson <thheopierson@gmail.com>
2024-05-04 10:49:25 +0200

    feat: make client calendar for appointment booking

--- src/app/pages/book-appointment/booking-form/booking-form.component.ts
+++ src/app/pages/book-appointment/booking-form/booking-form.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TypePrestation } from 'src/app/core/models/type_prestation';
+import { TypePrestationService } from 'src/app/core/services/type-prestation.service';
 
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css'],
 })
-export class BookingFormComponent {}
+export class BookingFormComponent {
+  typePrestation$: Observable<TypePrestation[]> = this.getTypePrestation();
+
+  constructor(private typePrestationService: TypePrestationService) {}
+
+  private getTypePrestation(): Observable<TypePrestation[]> {
+    return this.typePrestationService.fetchTypePrestation();
+  }
+}
