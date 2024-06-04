1aaff4450bc061cf3949da8b0969b93b11a59bbc
Theo Pierson <thheopierson@gmail.com>
2024-06-04 21:10:30 +0200

    feat: change backend url to test database connexion

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://localhost:8081',
-  apiBaseUrl: 'http://35.181.52.231:8081',
+  apiBaseUrl: 'http://localhost:8081',
+  //apiBaseUrl: 'http://35.181.52.231:8081',
+  //apiBaseUrl: 'http://15.237.40.208:8081',
 };
