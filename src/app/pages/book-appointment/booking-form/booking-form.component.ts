dfbe510b08412bb5a5e1d0110eda127a5f34d745
Theo Pierson <thheopierson@gmail.com>
2024-06-11 11:25:34 +0200

    feat: hours should be selected

--- src/app/pages/book-appointment/booking-form/booking-form.component.ts
+++ src/app/pages/book-appointment/booking-form/booking-form.component.ts
@@ -22,10 +22,10 @@ export class BookingFormComponent implements OnInit {
   minDate: Date = new Date();
   maxDate: Date;
 
-  selectedPrestation: string | undefined; // Nouvelle propriété pour stocker la valeur de l'input prestation
+  selectedPrestation: string | undefined;
   selectedDate: Date = new Date();
   dateToString: string = this.selectedDate.toLocaleDateString();
-  selectedHour: string = '10:00';
+  selectedHour: string | null = null; // Modifié pour accepter null
 
   customerId: number = 0;
   customerFirstname: string = '';
@@ -35,7 +35,7 @@ export class BookingFormComponent implements OnInit {
   customerBirthdate: string = '';
   customerPassword: string = '';
 
-  decodedToken: any; // Pour stocker les informations du JWT décrypté
+  decodedToken: any;
   takenHours: { start: string; end: string }[] = [];
 
   typePrestation$: Observable<TypePrestation[]> = this.getTypePrestation();
@@ -49,22 +49,24 @@ export class BookingFormComponent implements OnInit {
     public matDialog: MatDialog,
     private fb: FormBuilder
   ) {
-    // Ajouter 3 mois à maxDate
     this.maxDate = new Date(this.minDate);
     this.maxDate.setMonth(this.maxDate.getMonth() + 3);
   }
+
   ngOnInit(): void {
     console.log(this.dateToString);
 
     const accessToken = localStorage.getItem('access_token');
     if (accessToken) {
-      this.decodedToken = jwtDecode(accessToken); // Décoder le JWT
+      this.decodedToken = jwtDecode(accessToken);
       console.log('Informations du JWT décryptées :', this.decodedToken.id);
     }
   }
+
   private getTypePrestation(): Observable<Type_prestation[]> {
     return this.typePrestationService.fetchTypePrestation();
   }
+
   private getAppointments(): Observable<Appointments[]> {
     return this.appointmentService.fetchAppointments();
   }
@@ -74,16 +76,18 @@ export class BookingFormComponent implements OnInit {
     return day != 0 && day != 6;
   };
 
-  selectHour(hour: string) {
+  selectHour(hour: string): void {
     this.selectedHour = hour;
+  }
+
+  isHourSelected(hour: string): boolean {
+    return this.selectedHour === hour;
   }
 
   getCustomerById(id: string) {
     let idCustomer = this.decodedToken.id;
-    // Appel du service pour trouver le client par son identifiant
     this.customerService.findCustomerById(idCustomer).subscribe((customer) => {
       console.log('Informations du client :', customer);
-      // Assignation de l'ID du client à la variable customerId
       this.customerId = customer.id;
     });
   }
@@ -92,13 +96,10 @@ export class BookingFormComponent implements OnInit {
     let selectedPrestationDuration: number = 0;
     let selectedPrestationId: number = 0;
     if (this.selectedPrestation) {
-      // Séparation de la chaîne en utilisant le tiret comme délimiteur
       const parts = this.selectedPrestation.split('-');
-      // Extrait le nombre au début de la chaîne et le convertit en entier
       selectedPrestationDuration = parseInt(parts[0], 10);
       selectedPrestationId = parseInt(parts[1]);
-      // Utilisez selectedPrestationDuration comme bon vous semble
-      console.log(selectedPrestationDuration); // Affiche 20
+      console.log(selectedPrestationDuration);
     } else {
       console.log("selectedPrestation n'est pas défini");
     }
@@ -106,7 +107,6 @@ export class BookingFormComponent implements OnInit {
     console.log('Date sélectionnée :', this.selectedDate);
     console.log('Heure sélectionnée :', this.selectedHour);
 
-    // Construire la date de début
     let startDate: Date = new Date(this.selectedDate);
     if (this.selectedHour) {
       const [hours, minutes] = this.selectedHour.split(':');
@@ -114,14 +114,12 @@ export class BookingFormComponent implements OnInit {
     }
     console.log('La date de début :', startDate);
 
-    // Calculer la date de fin
     const endDate: Date = new Date(
       startDate.getTime() + selectedPrestationDuration * 60000
-    ); // Convertir les minutes en millisecondes (1 minute = 60000 millisecondes)
+    );
 
     console.log('La date de fin :', endDate);
 
-    // Appeler la méthode pour récupérer les informations du client
     this.getCustomerById(this.decodedToken.id);
     let selectedCustomerId = this.customerId;
     let selectedCustomerFirstname = this.customerFirstname;
@@ -145,28 +143,31 @@ export class BookingFormComponent implements OnInit {
         password: selectedCustomerPassword,
       },
     };
+
     this.appointmentService
       .addAppointment(appointmentObj)
       .subscribe((response: Appointments) => {});
+
     this.typePrestationService.findById(selectedPrestationId).subscribe(
       (res: TypePrestation) => {
         this.typePrestationIdService.setSelectedTypePrestationId(
           selectedPrestationId
         );
-        this.openModal(); // Ouvrez la modale avec les informations du client
+        this.openModal();
       },
       (error: HttpErrorResponse) => {
         console.error(error);
       }
     );
   }
+
   onDateChange(date: Date) {
-    const twentyHoursInMilliseconds = 20 * 60 * 60 * 1000; // 20 heures en millisecondes
+    const twentyHoursInMilliseconds = 20 * 60 * 60 * 1000;
     console.log('Date sélectionnée :', date);
 
-    const startDate: string = date.toISOString(); // Convertir la date de début en chaîne de caractères
+    const startDate: string = date.toISOString();
     const endDate: Date = new Date(date.getTime() + twentyHoursInMilliseconds);
-    const endDateStr: string = endDate.toISOString(); // Convertir la date de fin en chaîne de caractères
+    const endDateStr: string = endDate.toISOString();
 
     this.appointmentService.findByDate(startDate, endDateStr);
     this.isThereAppointmentOnDate(startDate, endDateStr);
@@ -175,12 +176,12 @@ export class BookingFormComponent implements OnInit {
   openModal() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.id = 'modal-component';
-    // https://material.angular.io/components/dialog/overview
     const modalDialog = this.matDialog.open(
       AppointmentModalComponent,
       dialogConfig
     );
   }
+
   isThereAppointmentOnDate(startDate: string, endDate: string) {
     this.appointmentService.findByDate(startDate, endDate).subscribe(
       (appointments: any[]) => {
@@ -209,6 +210,7 @@ export class BookingFormComponent implements OnInit {
       }
     );
   }
+
   isHourAvailable(hour: string): boolean {
     const [hourStart, minuteStart] = hour.split(':').map(Number);
     const start = new Date(this.selectedDate);
