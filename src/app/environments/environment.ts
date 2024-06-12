b4c65614f241b9f644bee5294e2925366a53f519
Theo Pierson <thheopierson@gmail.com>
2024-06-12 10:17:20 +0200

    feat: change url to frankfort server

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiBaseUrl: 'http://localhost:8081',
-  apiBaseUrl: 'https://onyx-backend-vrxw.onrender.com',
-  //apiBaseUrl: 'http://35.181.52.231:8081',
-  //apiBaseUrl: 'http://15.237.40.208:8081',
+
+  apiBaseUrl: 'https://onyx-v3.onrender.com',
+  //apiBaseUrl: '3.75.158.163',
 };
