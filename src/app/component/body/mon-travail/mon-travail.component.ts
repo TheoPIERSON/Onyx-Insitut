3e494c7e6fdccf6384cc094ec2923d551783451e
Theo Pierson <thheopierson@gmail.com>
2024-11-22 11:54:12 +0100

    feat: change type prestation list display

--- src/app/component/body/mon-travail/mon-travail.component.ts
+++ src/app/component/body/mon-travail/mon-travail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { Type_prestation } from 'src/app/core/classes/type_prestation_class';
 import { TypePrestation } from 'src/app/core/models/type_prestation';
 import { TypePrestationService } from 'src/app/core/services/type-prestation.service';
 
@@ -10,7 +11,18 @@ import { TypePrestationService } from 'src/app/core/services/type-prestation.ser
   styleUrls: ['./mon-travail.component.css'],
 })
 export class MonTravailComponent implements OnInit {
-  typePrestation$: Observable<TypePrestation[]> = this.getTypePrestation();
+  typePrestationGrouped$: Observable<Record<string, Type_prestation[]>> =
+    this.getTypePrestation().pipe(
+      map((prestations) =>
+        prestations.reduce((acc, prestation) => {
+          if (!acc[prestation.type]) {
+            acc[prestation.type] = [];
+          }
+          acc[prestation.type].push(prestation);
+          return acc;
+        }, {} as Record<string, Type_prestation[]>)
+      )
+    );
 
   constructor(
     private typePrestationService: TypePrestationService,
